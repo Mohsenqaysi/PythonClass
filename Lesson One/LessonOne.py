e7e9fe6c3705231e9a3393551a47de57285870cd
Petra Kovaru <petrakovaru@gmail.com>
2019-02-27 16:09:14 +0000

    Added folders

---
+++ Lesson One/LessonOne.py
@@ -0,0 +1,26 @@
+import random
+import sys
+import os
+
+# This is a String <text>
+name = "Mohsen"
+print(name)
+
+# This is a Nymber <Int> 
+age = 27
+print(age)
+# This is a rando number 
+
+print("1 + 2 - 3 * 2 = {} ".format( 1 + 2 - 3 * 2))
+print("(1 + 2 - 3) * 2 = {} ".format( (1 + 2 - 3) * 2))
+
+quote = "\"You are special\""
+print(quote)
+
+multi_line_quote = ''' just
+    like 
+        eveyone 
+else
+'''
+
+print(multi_line_quote + quote)
